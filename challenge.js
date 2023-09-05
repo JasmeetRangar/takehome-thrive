d70ef41de3450a06fd2e9bab3bdbef9a2dff09ae
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2023-09-05 17:38:51 -0400

    Sort users and companies

--- challenge.js
+++ challenge.js
@@ -1,11 +1,22 @@
+let users = [];
+let companies = [];
+
 try {
-  const users = require("./users.json");
+  users = require("./users.json");
 } catch (e) {
   throw new Error("users.json is not a valid json file");
 }
 
 try {
-  const companies = require("./companies.json");
+  companies = require("./companies.json");
 } catch (e) {
   throw new Error("companies.json is not a valid json file");
 }
+
+let sortedUsers = users.sort((u1, u2) =>
+  u1.last_name > u2.last_name ? 1 : u2.last_name > u1.last_name ? -1 : 0
+);
+
+let sortedCompanies = companies.sort((c1, c2) =>
+  c1.id > c2.id ? 1 : c2.id > c1.id ? -1 : 0
+);
